eacd7a8f8c8b2ff72cb7a711b3f2aef0b61be7a3
Aamir Mushtaq Siddiqui <33178990+TheKeySpammer@users.noreply.github.com>
2021-06-12 13:53:10 +0530

    fix database timezone

--- modules/database.js
+++ modules/database.js
@@ -1,27 +1,41 @@
-const fs = require('fs');
-const Sequelize = require('sequelize'),
-    sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASS, {
-        host: process.env.DBHOST,
-        dialect: 'mysql',
-         pool: {
-             max: 5,
-             min: 0,
-             acquire: 30000,
-             idle: 10000
-         },
-         timezone: '+05:30',
-        //  logging: msg => {
-        //     fs.appendFile('logs/sequelize_query.log', new Date().toDateString()+'\n'+msg+'\n\n', err => {
-        //         if (err) {
-        //             console.log("Logging sequelize query failed");
-        //             console.log(err);
-        //         }
-        //     });
-        //  },
-         logging: false,
-    });
+const fs = require("fs");
+const Sequelize = require("sequelize"),
+  sequelize = new Sequelize(
+    process.env.DBNAME,
+    process.env.DBUSER,
+    process.env.DBPASS,
+    {
+      host: process.env.DBHOST,
+      dialect: "mysql",
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+      timezone: '+05:45',
+      dialectOptions: {
+        dateStrings: true,
+        typeCast: function (field, next) { // for reading from database
+            if (field.type === 'DATETIME') {
+                return new Date(field.string())
+            }
+            return next()
+        },
+      },
+      //  logging: msg => {
+      //     fs.appendFile('logs/sequelize_query.log', new Date().toDateString()+'\n'+msg+'\n\n', err => {
+      //         if (err) {
+      //             console.log("Logging sequelize query failed");
+      //             console.log(err);
+      //         }
+      //     });
+      //  },
+      logging: false,
+    }
+  );
 
 module.exports = {
-    sequelize: sequelize,
-    Sequelize: Sequelize,
+  sequelize: sequelize,
+  Sequelize: Sequelize,
 };
